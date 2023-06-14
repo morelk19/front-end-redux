4169f5066211393e0bd4d9ff5a5d65e3cc272ddc
morelk19 <morelkhristian@gmail.com>
2023-06-14 13:59:13 -0400

    added login and register pages

---
+++ src/theme.js
@@ -0,0 +1,16 @@
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+    palette:{
+        primary:{
+            main: '#FFC6AC',
+        },
+        secondary:{
+            main: '#C4A29E'
+        },
+        error:{
+            main: '#EBA6A9',
+        }
+    }
+})
+export default theme
